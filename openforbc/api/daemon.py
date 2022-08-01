36d2a7f4513000ee9f6641d65366e0becdd722c5
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2022-08-01 16:53:57 +0200

    Fix uvicorn logs duplication

--- openforbc/api/daemon.py
+++ openforbc/api/daemon.py
@@ -1,3 +1,4 @@
+"""The OpenForBC daemon (API) server."""
 from os import environ as env
 from logging import basicConfig
 
@@ -5,9 +6,17 @@ LOG_LEVEL = env.get("OPENFORBCD_LOGLEVEL") or env.get("LOGLEVEL")
 basicConfig(level=LOG_LEVEL, format="%(name)s: %(levelname)s: %(message)s")
 
 
-def run():
+def run() -> None:
+    """Run the OpenForBC daemon."""
     from uvicorn import run as uv_run
+    from uvicorn.config import LOGGING_CONFIG
 
     from openforbc.api.server import app
 
-    uv_run(app, port=5000)
+    log_config = LOGGING_CONFIG.copy()
+    for name in ["uvicorn", "uvicorn.access", "uvicorn.error"]:
+        log_config["loggers"][name]["handlers"] = []
+        log_config["loggers"][name]["propagate"] = True
+        log_config["loggers"][name]["level"] = LOG_LEVEL or "INFO"
+
+    uv_run(app, port=5000, log_config=log_config)
